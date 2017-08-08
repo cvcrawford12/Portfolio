342e53016a3d52fc74f224362bf3a378e27e7b12
connorcrawford14@gmail.com <connorcrawford14@gmail.com>
2017-08-08 13:09:15 +0200

    Added typewriter animation, fixed iphone animation, starting next section

--- ClubsProposal/js/main.js
+++ ClubsProposal/js/main.js
@@ -1,4 +1,45 @@
 function main() {
+
+  // hide navbar until user starts scrolling
+  $('.navbar').hide();
+
+  var firstP = "<p class='content'>We believe that people are active by nature. We believe that people are at the <strong>center</strong> of every sport. We are <strong>committed</strong> to providing the <strong>highest quality</strong> solutions to establish an <strong>equilibrium</strong> between sports, innovation, technology, and people.</p>";
+  var secondP = '<p class="content">We work with dedication to provide a new <strong>alternative</strong> that allows you to experiment, enjoy, and live sports <strong>like never before.</strong></p>';
+
+  var innerText = firstP + secondP,
+    i = 0,
+    isTag,
+    text;
+
+
+  function type() {
+      text = innerText.slice(0, ++i);
+      if (text === innerText) return;
+
+      document.querySelector('.typewriter').innerHTML = text;
+
+      var char = text.slice(-1);
+      if( char === '<' ) isTag = true;
+      if( char === '>' ) isTag = false;
+
+      if (isTag) return type();
+      setTimeout(type, 10);
+  }
+
+  // if we aren't mobile then perform type animation
+  var windowSize = window.screen.width;
+  if (windowSize > 800) {
+    type();
+  }
+
+  // if we resize from mobile to desktop then make sure text stays
+  $(window).on('resize', function() {
+    document.querySelector('.typewriter').innerHTML = innerText;
+  });
+
+
+  // For the what section hide all the content and then show only first li
+  // This will be the club li item
   var iphoneImages = document.querySelectorAll('.iphone-image');
   var whatContent = document.querySelectorAll('.what-content');
   $(iphoneImages).hide();
@@ -6,14 +47,19 @@ function main() {
   $(iphoneImages[0]).show();
   $(whatContent[0]).show();
 
+  // add click events on all filter buttons
   function handleBtnFilter() {
     var filterBtns = document.querySelectorAll('.filter-btn');
     filterBtns.forEach(function(btn) {
       btn.addEventListener('click', function(e) {
+        // get id of btn with active class
+        // get id of btn which was clicked
         var currentId = $('.active').attr('id');
         var nextId = btn.id;
         $('#what .active').removeClass('active');
         $(btn).addClass('active');
+
+        // take the current active content and animate to next based on which button was clicked
         $('#' + currentId + '-content').attr('class', 'what-content animated fadeOut');
         $('#' + currentId + '-content').hide();
         $('#' + nextId + '-content').attr('class', 'what-content animated fadeIn');
@@ -28,7 +74,20 @@ function main() {
 
   handleBtnFilter();
 
-  // Page scroll
+  // Fade in .navbar
+	$(function () {
+		$(window).scroll(function () {
+      // set distance user needs to scroll before we fadeIn navbar
+			if ($(this).scrollTop() > 200) {
+				$('.navbar').fadeIn();
+			} else {
+				$('.navbar').fadeOut();
+			}
+		});
+
+	});
+
+  // Page scroll, smooth scroll to location which anchor is clicked
    $('a.page-scroll').click(function() {
        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
          var target = $(this.hash);
